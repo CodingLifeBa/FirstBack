539c66ccdd60e0dc5411f89c8277a0650711ebdc
Amane_Ba <121307431+CodingLifeBa@users.noreply.github.com>
2024-06-24 15:15:18 +0000

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,19 @@ app.use(cors({
   credentials: true
 }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 
   
   next();
