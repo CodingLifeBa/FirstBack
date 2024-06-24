4f01910673b5c72f5869ca7fb0b2dd360f80bbfd
Amane_Ba <121307431+CodingLifeBa@users.noreply.github.com>
2024-06-24 15:22:46 +0000

    Update app.js

--- app.js
+++ app.js
@@ -17,22 +17,11 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-app.use(cors({
-  origin: ["https://first-back-sigma.vercel.app", "https://front-red-product.vercel.app"],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true
-}));
-
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "https://first-back-sigma.vercel.app, https://front-red-product.vercel.app");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
 
