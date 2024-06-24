0177aa611a2370c1c8753416749eae8cb6bdbf9f
Amane_Ba <121307431+CodingLifeBa@users.noreply.github.com>
2024-06-24 17:35:42 +0000

    Update app.js

--- app.js
+++ app.js
@@ -20,18 +20,12 @@ mongoose
 
 app.use(cors()); // Utilisez le middleware CORS ici
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+app.use(cors({
+  origin: ["https://first-back-sigma.vercel.app", "https://front-red-product.vercel.app"],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+  credentials: true
+}));
 
 
   
